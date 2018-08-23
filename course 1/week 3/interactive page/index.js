7baeecb963446d0cb5e910553b7090d5473b2ae7
Omar Dahir <omardahir@Omars-MacBook-Pro.local>
2018-08-22 21:20:10 -0600

    added week 3

---
+++ course 1/week 3/interactive page/index.js
@@ -0,0 +1,14 @@
+var d1 = document.getElementById('div1')
+var d2 = document.getElementById('div2')
+var body = document.getElementById('body')
+function changeColor() {
+  d1.className = "colorRed"
+  d2.className = "colorGreen"
+}
+function changeText() {
+  d1.innerHTML = "Kiwi"
+  d2.innerHTML = "Mango"
+}
+function dancingBannana() {
+  body.className = "bannana"
+}
